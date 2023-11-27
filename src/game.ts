f6d39891ce0281e8c5ae244e5fc2811ca4c142a5
Al Bondad <albondad@gmail.com>
2023-11-27 15:50:38 -0600

    Update game

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,5 @@
 import * as Phaser from "phaser";
+import { Tile } from "./types";
 
 const gameWidth = 1280;
 const gameHeight = 720;
@@ -7,6 +8,10 @@ const gameXCenter = gameWidth / 2;
 export default class MainScene extends Phaser.Scene {
   startTime: Number;
   tempSprite: Phaser.GameObjects.Sprite;
+  tileRows: Tile[][] = [];
+  tileSpritesGroup: Phaser.GameObjects.Group;
+  tileSpritesLayer: Phaser.GameObjects.Layer;
+  goButtonSpriteLayer: Phaser.GameObjects.Layer;
 
   constructor() {
     super("mainScene");
@@ -21,6 +26,15 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    this.tileSpritesGroup = this.add.group();
+    this.tileSpritesLayer = this.add.layer();
+    this.tileSpritesLayer.setDepth(1);
+    const barSpriteLayer = this.add.layer();
+    barSpriteLayer.setDepth(2);
+    this.goButtonSpriteLayer = this.add.layer();
+    this.goButtonSpriteLayer.setDepth(3);
+
+    this.setUpTileRows();
     // set bup bar sprite layer
     const topBarSprite = this.add.sprite(0, 0, "topBarImage");
     topBarSprite.setOrigin(0, 0);
@@ -29,103 +43,141 @@ export default class MainScene extends Phaser.Scene {
     bottomBarSprite.setOrigin(0, 0);
     bottomBarSprite.setY(448);
 
-    const barSpriteLayer = this.add.layer();
-
     barSpriteLayer.add(topBarSprite);
     barSpriteLayer.add(bottomBarSprite);
 
-    barSpriteLayer.setVisible(true);
-
-    const goButtonSprite = this.add
-      .sprite(gameXCenter, 592, "buttonImage")
-      .setInteractive();
-
-    let boardTiles = [[""]];
-
-    goButtonSprite.on("pointerup", () => {
-      // populate tiles
-      const boardSize = 3;
-      let tileY = 128;
-      for (let rowIndex = 0; rowIndex < boardSize; rowIndex++) {
-        let tileX = 512;
-        boardTiles[rowIndex] = [];
-        for (let columnIndex = 0; columnIndex < boardSize; columnIndex++) {
-          const tileValue = Math.random() < 0.5 ? "x" : "o";
-          boardTiles[rowIndex][columnIndex] = tileValue;
-          if (tileValue === "x") {
-            this.add.sprite(tileX, tileY, "xImage");
-          } else {
-            this.add.sprite(tileX, tileY, "oImage");
-          }
-          tileX += 128;
-        }
-        tileY += 128;
-      }
-
-      // check matches
-      // check horizontal matches
-      const matches = [];
-      for (let row = 0; row < boardSize; row++) {
-        let isMatchingRow = true;
-        for (let column = 1; column < boardSize; column++) {
-          if (boardTiles[row][column] !== boardTiles[row][column - 1]) {
-            isMatchingRow = false;
-          }
-        }
-        if (isMatchingRow) {
-          matches.push(`row ${row + 1} is a match`);
-        }
-      }
-
-      // check for vertical matches
-      for (let column = 0; column < boardSize; column++) {
-        let isMatchingColumn = true;
-        for (let row = 1; row < boardSize; row++) {
-          if (boardTiles[row][column] !== boardTiles[row - 1][column]) {
-            isMatchingColumn = false;
-          }
-        }
-        if (isMatchingColumn) {
-          matches.push(`column ${column + 1} is a match`);
-        }
-      }
-
-      // check for top-left to bottom-right diagonal match
-      let hasTopLeftToBottomRight = true;
-      for (let index = 1; index < boardSize; index++) {
-        if (boardTiles[index][index] !== boardTiles[index - 1][index - 1]) {
-          hasTopLeftToBottomRight = false;
-        }
-      }
-      if (hasTopLeftToBottomRight) {
-        matches.push("has top left to bottom right match");
-      }
-
-      // check for bottom-left to top-right diagonal match
-      let hasBottomLeftToTopRightMatch = true;
-      for (let index = 1; index < boardSize; index++) {
-        if (
-          boardTiles[boardSize - 1 - index][index] !==
-          boardTiles[boardSize - index][index - 1]
-        ) {
-          hasBottomLeftToTopRightMatch = false;
-        }
-      }
-      if (hasBottomLeftToTopRightMatch) {
-        matches.push("has bottom left to top right match");
-      }
-
-      console.log(boardTiles);
-      console.log(matches);
-    });
-
-    this.tempSprite = this.add.sprite(120, 120, "xImage");
+    this.setupGoButtonLayer();
   }
 
   update() {
-    if (this.tempSprite.y <= 512) {
-      this.tempSprite.y += 50;
+    this.updateTileSpritesPosition();
+  }
+
+  setupGoButtonLayer() {
+    const goButtonSprite = this.add.sprite(0, 0, "buttonImage");
+    goButtonSprite.setOrigin(0, 0);
+    goButtonSprite.setInteractive();
+    const newGoButtonSpriteX = gameXCenter - goButtonSprite.width / 2;
+    goButtonSprite.setX(newGoButtonSpriteX);
+    goButtonSprite.setY(528);
+
+    goButtonSprite.on(Phaser.Input.Events.POINTER_OVER, () => {
+      goButtonSprite.setAlpha(0.75);
+    });
+
+    goButtonSprite.on(Phaser.Input.Events.POINTER_DOWN, () => {
+      goButtonSprite.setAlpha(0.5);
+      this.refreshTileRows();
+      this.refreshTileSprites();
+    });
+
+    goButtonSprite.on(Phaser.Input.Events.POINTER_UP, () => {
+      goButtonSprite.setAlpha(0.75);
+    });
+
+    goButtonSprite.on(Phaser.Input.Events.POINTER_OUT, () => {
+      goButtonSprite.setAlpha(1);
+    });
+
+    const goButtonLayer = this.add.layer();
+    this.goButtonSpriteLayer.add(goButtonSprite);
+  }
+
+  refreshTileSprites() {
+    this.tileSpritesGroup.children.each((child) => child.destroy());
+    let spriteY = 320 - (this.tileRows.length - 1) * 128;
+
+    this.tileRows.forEach((tileRow) => {
+      let spriteX = 448;
+
+      tileRow.forEach((tile) => {
+        const spriteTexture = this.getTileValueByTileTexture(tile.value);
+        const sprite = this.add.sprite(0, 0, spriteTexture);
+        sprite.setOrigin(0, 0);
+        sprite.setPosition(spriteX, spriteY);
+        this.tileSpritesGroup?.add(sprite);
+        this.tileSpritesLayer?.add(sprite);
+        spriteX += 128;
+      });
+
+      spriteY += 128;
+    });
+  }
+
+  updateTileSpritesPosition() {
+    console.log();
+    this.tileSpritesGroup?.children.iterate((child, index) => {
+      const isFirstRow = index === 0 || index === 1 || index === 2;
+      const isSecondRow = index === 3 || index === 4 || index === 5;
+      const isThirdRow = index === 6 || index === 7 || index === 8;
+      const isOtherRow = index >= 9;
+
+      if (isFirstRow && child.y < 64) {
+        child.y += 64;
+      } else if (isSecondRow && child.y < 192) {
+        child.y += 64;
+      } else if (isThirdRow && child.y < 320) {
+        child.y += 64;
+      } else if (isOtherRow && child.y <= 448) {
+        child.y += 64;
+      }
+    });
+  }
+
+  refreshTileRows() {
+    const newTileRows = [];
+    for (let i = 0; i < 27; i++) {
+      const randomTileRow = this.getRandomTileRow();
+      newTileRows.push(randomTileRow);
     }
+    for (let i = 0; i < 3; i++) {
+      const oldTileRow = this.tileRows[i];
+      newTileRows.push(oldTileRow);
+    }
+    this.tileRows = newTileRows;
+  }
+
+  setUpTileRows() {
+    for (let i = 0; i < 3; i++) {
+      const randomTileRow = this.getRandomTileRow();
+      this.tileRows.push(randomTileRow);
+    }
+  }
+
+  getRandomTileRow() {
+    const randomTilesRow: Tile[] = [];
+    for (let i = 0; i < 3; i++) {
+      const randomTile = this.getRandomTile();
+      randomTilesRow.push(randomTile);
+    }
+    return randomTilesRow;
+  }
+
+  getRandomTile() {
+    const randomTileValue = this.getRandomTileValue();
+    const randomTile = {
+      value: randomTileValue,
+    };
+    return randomTile;
+  }
+
+  getRandomTileValue() {
+    const randomValue = Math.random();
+    const randomTileValue = randomValue <= 0.5 ? "x" : "o";
+    return randomTileValue;
+  }
+
+  getTileValueByTileTexture(tileValue) {
+    let tileTexture = "";
+    switch (tileValue) {
+      case "o":
+        tileTexture = "oImage";
+        break;
+      case "x":
+        tileTexture = "xImage";
+        break;
+    }
+    return tileTexture;
   }
 }
 
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Tile = {
+  type?: string;
+  value?: string;
+};
